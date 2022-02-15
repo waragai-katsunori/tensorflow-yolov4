be9b5e05ccee0dccbf0aa272dec5d972b383c843
katsunori waragai <katsunori.waragai@groove-x.com>
2022-02-15 17:14:12 +0900

    use with tf.Graph().as_default():

--- py_src/yolov4/tf/__init__.py
+++ py_src/yolov4/tf/__init__.py
@@ -50,135 +50,136 @@ for i in range(len(physical_devices)):
 
 tf.enable_eager_execution()
 
-class YOLOv4(BaseClass):
-    @property
-    def model(self) -> YOLOv4Model:
-        return self._model
+with tf.Graph().as_default():
+    class YOLOv4(BaseClass):
+        @property
+        def model(self) -> YOLOv4Model:
+            return self._model
 
-    def make_model(self):
-        K.clear_session()
-        _input = Input(self.config.net.input_shape)
-        self._model = YOLOv4Model(config=self.config)
-        self._model(_input)
+        def make_model(self):
+            K.clear_session()
+            _input = Input(self.config.net.input_shape)
+            self._model = YOLOv4Model(config=self.config)
+            self._model(_input)
 
-    def load_weights(self, weights_path: str, weights_type: str = "tf"):
-        """
-        Usage:
-            yolo.load_weights("checkpoints")
-            yolo.load_weights("yolov4.weights", weights_type="yolo")
-        """
-        if weights_type == "yolo":
-            _load_weights(self._model, weights_path)
-        elif weights_type == "tf":
-            self._model.load_weights(weights_path)
+        def load_weights(self, weights_path: str, weights_type: str = "tf"):
+            """
+            Usage:
+                yolo.load_weights("checkpoints")
+                yolo.load_weights("yolov4.weights", weights_type="yolo")
+            """
+            if weights_type == "yolo":
+                _load_weights(self._model, weights_path)
+            elif weights_type == "tf":
+                self._model.load_weights(weights_path)
 
-    def save_weights(
-        self, weights_path: str, weights_type: str = "tf", to: int = 0
-    ):
-        """
-        Usage:
-            yolo.save_weights("checkpoints")
-            yolo.save_weights("yolov4.weights", weights_type="yolo")
-            yolo.save_weights("yolov4.conv.137", weights_type="yolo", to=137)
-        """
-        to_layer = ""
-        if to > 0:
-            to_layer = self.config.metalayers[to - 1].name
+        def save_weights(
+            self, weights_path: str, weights_type: str = "tf", to: int = 0
+        ):
+            """
+            Usage:
+                yolo.save_weights("checkpoints")
+                yolo.save_weights("yolov4.weights", weights_type="yolo")
+                yolo.save_weights("yolov4.conv.137", weights_type="yolo", to=137)
+            """
+            to_layer = ""
+            if to > 0:
+                to_layer = self.config.metalayers[to - 1].name
 
-        if weights_type == "yolo":
-            _save_weights(self._model, weights_path, to=to_layer)
-        elif weights_type == "tf":
-            self._model.save_weights(weights_path)
+            if weights_type == "yolo":
+                _save_weights(self._model, weights_path, to=to_layer)
+            elif weights_type == "tf":
+                self._model.save_weights(weights_path)
 
-    def summary(self, line_length=90, summary_type: str = "tf", **kwargs):
-        if summary_type == "tf":
-            self._model.summary(line_length=line_length, **kwargs)
-        else:
-            self.config.summary()
+        def summary(self, line_length=90, summary_type: str = "tf", **kwargs):
+            if summary_type == "tf":
+                self._model.summary(line_length=line_length, **kwargs)
+            else:
+                self.config.summary()
 
-    #############
-    # Inference #
-    #############
+        #############
+        # Inference #
+        #############
 
-    @tf.function
-    def _predict(self, x):
-        return self._model(x, training=False)
+        @tf.function
+        def _predict(self, x):
+            return self._model(x, training=False)
 
-    def predict(self, frame: np.ndarray, prob_thresh: float):
-        """
-        Predict one frame
+        def predict(self, frame: np.ndarray, prob_thresh: float):
+            """
+            Predict one frame
 
-        @param `frame`: Dim(height, width, channels)
-        @param `prob_thresh`
+            @param `frame`: Dim(height, width, channels)
+            @param `prob_thresh`
 
-        @return pred_bboxes
-            Dim(-1, (x, y, w, h, cls_id, prob))
-        """
-        # image_data == Dim(1, input_size[1], input_size[0], channels)
-        height, width, _ = frame.shape
+            @return pred_bboxes
+                Dim(-1, (x, y, w, h, cls_id, prob))
+            """
+            # image_data == Dim(1, input_size[1], input_size[0], channels)
+            height, width, _ = frame.shape
 
-        image_data = self.resize_image(frame)
-        image_data = image_data / 255.0
-        image_data = image_data[np.newaxis, ...].astype(np.float32)
+            image_data = self.resize_image(frame)
+            image_data = image_data / 255.0
+            image_data = image_data[np.newaxis, ...].astype(np.float32)
 
-        candidates = self._predict(image_data)
-        candidates = [
-            c.numpy().astype(np.float32, copy=False) for c in candidates
-        ]
+            candidates = self._predict(image_data)
+            candidates = [
+                c.numpy().astype(np.float32, copy=False) for c in candidates
+            ]
 
-        pred_bboxes = self.get_yolo_detections(
-            yolos=candidates, prob_thresh=prob_thresh
-        )
-        self.fit_to_original(pred_bboxes, height, width)
-        return pred_bboxes
+            pred_bboxes = self.get_yolo_detections(
+                yolos=candidates, prob_thresh=prob_thresh
+            )
+            self.fit_to_original(pred_bboxes, height, width)
+            return pred_bboxes
 
-    ############
-    # Training #
-    ############
+        ############
+        # Training #
+        ############
 
-    def compile(
-        self,
-        optimizer=None,
-        loss=None,
-        **kwargs,
-    ):
-        if optimizer is None:
-            optimizer = Adam(learning_rate=self.config.net.learning_rate)
-
-        if loss is None:
-            loss = YOLOv4Loss(config=self.config, model=self.model)
-
-        return self._model.compile(
-            optimizer=optimizer,
-            loss=loss,
+        def compile(
+            self,
+            optimizer=None,
+            loss=None,
             **kwargs,
-        )
+        ):
+            if optimizer is None:
+                optimizer = Adam(learning_rate=self.config.net.learning_rate)
 
-    def fit(
-        self,
-        dataset,
-        callbacks=None,
-        validation_data=None,
-        validation_steps=None,
-        verbose: int = 3,
-        **kwargs,
-    ):
-        """
-        verbose=3 is one line per step
-        """
-        callbacks = callbacks or []
-        callbacks.append(
-            YOLOCallbackAtEachStep(config=self.config, verbose=verbose)
-        )
+            if loss is None:
+                loss = YOLOv4Loss(config=self.config, model=self.model)
 
-        epochs = self.config.net.max_batches // len(dataset) + 1
+            return self._model.compile(
+                optimizer=optimizer,
+                loss=loss,
+                **kwargs,
+            )
 
-        return self._model.fit(
+        def fit(
+            self,
             dataset,
-            epochs=epochs,
-            verbose=verbose if verbose < 3 else 0,
-            callbacks=callbacks,
-            validation_data=validation_data,
-            validation_steps=validation_steps,
+            callbacks=None,
+            validation_data=None,
+            validation_steps=None,
+            verbose: int = 3,
             **kwargs,
-        )
+        ):
+            """
+            verbose=3 is one line per step
+            """
+            callbacks = callbacks or []
+            callbacks.append(
+                YOLOCallbackAtEachStep(config=self.config, verbose=verbose)
+            )
+
+            epochs = self.config.net.max_batches // len(dataset) + 1
+
+            return self._model.fit(
+                dataset,
+                epochs=epochs,
+                verbose=verbose if verbose < 3 else 0,
+                callbacks=callbacks,
+                validation_data=validation_data,
+                validation_steps=validation_steps,
+                **kwargs,
+            )
